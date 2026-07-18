6d1caebfabb9726f2753204a9c63d8b27ce11cfe
jazaki <ja2005222@gmail.com>
2026-07-18 17:14:54 +0200

    Refactor Tailwind config to use ES module syntax

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // هذا السطر يغطي كل ملفات الـ JS/JSX داخل مجلد src
-    "./public/index.html",         // إذا كنت تستخدم ملف HTML خارج مجلد src
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
